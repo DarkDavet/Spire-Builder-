078ff98d38001f94b0b4fd49860dbbcefb63c71e
DarkDavet <darkdavet@gmail.com>
2024-08-14 17:06:44 +0300

    Add existing project files

---
+++ Assets/Scripts/CanvasButton.cs
@@ -0,0 +1,62 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasButton : MonoBehaviour
+{
+    public Sprite musicOn, musicOff;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetString("music") == "No" && gameObject.name == "Music")
+            GetComponent<Image>().sprite = musicOff;
+    }
+
+    public void RestartGame()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    } 
+
+    public void LoadInstagram()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        Application.OpenURL("https://instagram.com/olnerd97?igshid=NTA5ZTk1NTc=");
+    }
+
+    public void LoadShop()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void CloseShop()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        SceneManager.LoadScene("Main");
+    }
+
+    public void MusicWork()
+    {
+        if(PlayerPrefs.GetString("music") == "No")
+        {
+            GetComponent<AudioSource>().Play();
+            PlayerPrefs.SetString("music", "Yes");
+            GetComponent<Image>().sprite = musicOn;
+        }
+        else
+        {
+            PlayerPrefs.SetString("music", "No");
+            GetComponent<Image>().sprite = musicOff;
+        }
+    }
+}
